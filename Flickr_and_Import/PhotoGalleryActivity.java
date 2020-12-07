8f76ba7e282e00a2bcae481030bf29bc777fc0c9
gursmeepsyan <gursmeep@gmail.com>
2020-12-08 02:04:53 +0530

    Add files via upload

---
+++ Flickr_and_Import/PhotoGalleryActivity.java
@@ -0,0 +1,15 @@
+/*
+Description: makes the fragment for the flickr photos, taken from the book source code
+ */
+package com.example.finddamatch.Flickr_and_Import;
+
+
+import androidx.fragment.app.Fragment;
+
+public class PhotoGalleryActivity extends Single_Fragment_Activity {
+
+    @Override
+    protected Fragment createFragment() {
+        return Photo_Gallery_Fragment.newInstance();
+    }
+}
